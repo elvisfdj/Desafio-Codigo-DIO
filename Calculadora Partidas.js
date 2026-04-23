9ecc9a182580418e98a3d049d42d4c9316c94996
elvisfdj <elvis.fdj@aol.com>
2026-04-23 01:57:12 -0300

    Update

---
+++ Calculadora Partidas.js
@@ -0,0 +1,27 @@
+console.log("Desafio do Herói");
+let heroName = "Dave Oliveira";
+let heroWins = 9000;
+const msg = "O  Herói de nome "
+const msgF = " está no nível: "
+
+if (heroWins < 10){
+console.log(msg + heroName + msgF + "Ferro" )    
+}
+else if (heroWins <= 20){
+console.log(msg + heroName + msgF + "Bronze")    
+}
+else if (heroWins <= 50){
+console.log(msg + heroName + msgF + "Prata")    
+}
+else if (heroWins <= 80){
+console.log(msg + heroName + msgF + "Ouro")    
+}
+else if (heroWins <= 90){
+console.log(msg + heroName + msgF + "Diamante")    
+}
+else if (heroWins <= 100){
+console.log(msg + heroName + msgF + "Lendário")    
+}
+else if (heroWins <= 101){
+console.log(msg + heroName + msgF + "Imortal")    
+}
